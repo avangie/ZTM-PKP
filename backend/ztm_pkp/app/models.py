b9f7eda1c265217b38004b94427cf46d92bd9a6b
CyferekCyfur <kubapro84@gmail.com>
2024-01-20 09:49:52 +0100

    added proper user fields

--- backend/ztm_pkp/app/models.py
+++ backend/ztm_pkp/app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class User(models.Model):
     user_id = models.CharField(max_length=20)
     user_name = models.CharField(max_length=20)
+    user_password = models.CharField(max_length=200)
 
 
 class GameStateXO(models.Model):
